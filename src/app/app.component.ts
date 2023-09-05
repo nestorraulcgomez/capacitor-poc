089ec34cca4cf130fb81e393d4b4b5a10354a14d
Nestor Castillo Gomez <ncastillog@MACBOOK-PRO-DE-RI704804.local>
2023-09-05 16:13:03 -0300

    add smartlook and compatibility with java 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Smartlook from 'cordova-plugin-smartlook';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    Smartlook.setProjectKey({key: '9b73e83febd539d3b3e7f2cec80c1f7ff8706e75'});
+    Smartlook.setWebViewSensitivity({ isSensitive: true });
+    Smartlook.start();
+  }
 }
