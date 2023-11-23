ce1caa0cbb04e0f40fcfc5206eec7e5f5a33af37
Nestor Castillo Gomez <ncastillog@MACBOOK-PRO-DE-RI704804.local>
2023-11-23 16:48:42 -0300

    pwa libraries added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as Smartlook from 'cordova-plugin-smartlook';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +9,5 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    Smartlook.setProjectKey({key: '9b73e83febd539d3b3e7f2cec80c1f7ff8706e75'});
-    Smartlook.setWebViewSensitivity({ isSensitive: true });
-    Smartlook.start();
   }
 }
